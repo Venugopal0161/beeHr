bca0c2316157e91b9d19f0c953add4a76725b4a2
venugopal <venugopal0161@gmail.com>
2025-09-22 13:28:39 +0530

    fix: :sparkles: 17

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,24 +4,26 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DecreaseTextPipe } from 'src/app/pipe/decrease-text.pipe';
 // import { AlphaNumericDirective } from '../directives/alpha-numeric.directive';
 // import { CaseSensitiveDirective } from '../directives/casesensitive.directive';
-import { ButtonModule } from 'primeng/button';
-import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { FileUploadModule } from 'primeng/fileupload';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { RatingModule } from 'primeng/rating';
-import { RippleModule } from 'primeng/ripple';
-import { TableModule } from 'primeng/table';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { TabViewModule } from 'primeng/tabview';
-import { ToastModule } from 'primeng/toast';
-import { ToolbarModule } from 'primeng/toolbar';
+ 
 import { SafeHtmlPipe } from '../pipe/safeHtml.pipe';
 import { sortBySortOrderPipe } from '../pipe/sortByOrderForTax';
 import { TimeFormaterPipe } from '../pipe/time-formater.pipe';
+
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { DialogModule } from 'primeng/dialog';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { RatingModule } from 'primeng/rating';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 
 @NgModule({
